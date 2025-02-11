4b80c59b6530075349f36ca9cd4b0a84a6544f8d
Ankita Arya <ankitaarya1207@gmail.com>
2025-02-12 00:21:32 +0530

    work till header

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+  <div>
+    Hello from Ankita
+    <br/>
+    <Button>Hello</Button>
+  </div>
+  );
+}
