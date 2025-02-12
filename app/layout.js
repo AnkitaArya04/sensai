d41beb5b577956354adc7fd05be89a4f5b28f3a7
Ankita Arya <ankitaarya1207@gmail.com>
2025-02-12 21:49:10 +0530

    changes the font

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter=Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,6 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+      
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -44,5 +39,6 @@ export default function RootLayout({ children }) {
           </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
