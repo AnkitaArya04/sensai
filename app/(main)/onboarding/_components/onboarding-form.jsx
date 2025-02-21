1735c544216d1cbe609fe7937730c89858999438
Ankita Arya <ankitaarya1207@gmail.com>
2025-02-21 17:16:21 +0530

    issue solveed till onboarding page

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,45 +1,14 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2 } from "lucide-react";
-import { toast } from "sonner";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import useFetch from "@/hooks/use-fetch";
 import { onboardingSchema } from "@/app/lib/schema";
-import { updateUser } from "@/actions/user";
+const OnboardingForm = ({industries}) => {
 
-const OnboardingForm = ({ industries }) => {
-  const router = useRouter();
   const [selectedIndustry, setSelectedIndustry] = useState(null);
-
-  const {
-    loading: updateLoading,
-    fn: updateUserFn,
-    data: updateResult,
-  } = useFetch(updateUser);
-
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -48,32 +17,7 @@ const OnboardingForm = ({ industries }) => {
     watch,
   } = useForm({
     resolver: zodResolver(onboardingSchema),
-  });
-
-  const onSubmit = async (values) => {
-    try {
-      const formattedIndustry = `${values.industry}-${values.subIndustry
-        .toLowerCase()
-        .replace(/ /g, "-")}`;
-
-      await updateUserFn({
-        ...values,
-        industry: formattedIndustry,
-      });
-    } catch (error) {
-      console.error("Onboarding error:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (updateResult?.success && !updateLoading) {
-      toast.success("Profile completed successfully!");
-      router.push("/dashboard");
-      router.refresh();
-    }
-  }, [updateResult, updateLoading]);
-
-  const watchIndustry = watch("industry");
+  })
   return (
     <div>
       OnboardingForm
