f17de152342fcaedab1e38d1d658560e3ab78b20
Ankita Arya <ankitaarya1207@gmail.com>
2025-02-24 19:21:25 +0530

    cron jo configured

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 
 
 import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 // import { generateIndustryInsights } from "@/lib/inngest/function";
 
 // Create an API that serves zero functions
@@ -9,6 +10,7 @@ export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
       /* your functions will be passed here later! */
+      generateIndustryInsights,
    
     ],
   });
